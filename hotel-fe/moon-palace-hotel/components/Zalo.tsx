b4b7f438b63e1a3fd6ee5b4ad3d6db3eb3df187b
kiendeptrai <kienn2245@gmail.com>
2026-03-25 08:19:13 +0700

    zaloconfig

--- hotel-fe/moon-palace-hotel/components/Zalo.tsx
+++ hotel-fe/moon-palace-hotel/components/Zalo.tsx
@@ -8,7 +8,7 @@ const ZaloButton: React.FC = () => {
       href={`https://zalo.me/${phoneNumber}`}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-36 right-7 z-50 flex items-center justify-center w-14 h-14 bg-blue-500 rounded-full shadow-lg hover:scale-110 hover:bg-blue-600 transition-all duration-300"
+      className="fixed bottom-36 right-7 z-40 flex items-center justify-center w-14 h-14 bg-blue-500 rounded-full shadow-lg hover:scale-110 hover:bg-blue-600 transition-all duration-300"
     >
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/9/91/Icon_of_Zalo.svg"
